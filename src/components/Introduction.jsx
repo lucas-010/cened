e0d78a3682fe673313adb9220949b9caba8bbada
vctg1 <victormourago@gmail.com>
2022-06-07 12:42:47 -0300

    'home-responsive-fix'

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -27,48 +27,48 @@ export default function Intro() {
   return (
     <div className='mt-20 lg:p-10 lg:mt-36 lg:text-2xl' style={{background: 'linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(240,230,140,1) 100%)'}}>
         <h1 className='text-center font-bold lg:text-4xl'>PROGRAMA DE QUALIFICAÇÃO PROFISSIONAL ÀS PESSOAS PRIVADAS DE LIBERDADE</h1>
-        <div className='flex mt-10 text-sm text-center lg:text-xl lg:font-bold'>
-            <div className='flex justify-between flex-col items-center w-1/4 h-52 lg:h-96'>
-            <button className='lg:w-2/3 hover:w-3/4 hover:transition-all h-fit' onClick={() => { allClosed ? setOpen1(true) : open1 ? setOpen1(false) : setTimeout(function(){setOpen1(true)}, 450); 
-            setOpen2(false);setOpen3(false);setOpen4(false);}}><img src={slideImg1} alt='' /></button>
+        <div className='flex lg:mt-10 mt-4 text-xs text-center lg:text-xl lg:font-bold'>
+            <div className='flex flex-col justify-between items-center w-1/4 h-40 lg:h-96'>
             <p>Reconstruindo a vida!</p>
+            <button className='w-2/3 hover:w-3/4 hover:lg:transition-all h-fit flex flex-col' onClick={() => { allClosed ? setOpen1(true) : open1 ? setOpen1(false) : setTimeout(function(){setOpen1(true)}, 450); 
+            setOpen2(false);setOpen3(false);setOpen4(false);}}><img src={slideImg1} alt='' /><p className='text-white mt-2 text-xs lg:text-base hover:bg-green-500 hover:lg:transition-colors rounded-xl pl-2 pr-2 bg-blue-800 w-fit place-self-center'>SAIBA +</p></button>
             </div>
-            <div className='flex justify-between flex-col items-center h-52 lg:h-96 w-1/4'>
-            <button className='lg:w-2/3 hover:w-3/4 hover:transition-all h-fit' onClick={() => { allClosed ? setOpen2(true) : open2 ? setOpen2(false) : setTimeout(function(){setOpen2(true)}, 450); 
-            setOpen1(false);setOpen3(false);setOpen4(false);}}> <img src={slideImg2} alt='' /> </button>
+            <div className='flex flex-col justify-between items-center h-40 lg:h-96 w-1/4'>
             <p>Educação para todos!</p>
+            <button className='w-2/3 hover:w-3/4 hover:lg:transition-all h-fit flex flex-col' onClick={() => { allClosed ? setOpen2(true) : open2 ? setOpen2(false) : setTimeout(function(){setOpen2(true)}, 450); 
+            setOpen1(false);setOpen3(false);setOpen4(false);}}> <img src={slideImg2} alt='' /><p className='text-white mt-2 text-xs lg:text-base hover:bg-green-500 hover:lg:transition-colors rounded-xl pl-2 pr-2 bg-blue-800 w-fit place-self-center'>SAIBA +</p></button> 
             </div>
-            <div className='flex justify-between flex-col items-center h-52 lg:h-96 w-1/4'>
-            <button className='lg:w-2/3 hover:w-3/4 hover:transition-all h-fit' onClick={() => { allClosed ? setOpen3(true) : open3 ? setOpen3(false) : setTimeout(function(){setOpen3(true)}, 450); 
-            setOpen1(false);setOpen2(false);setOpen4(false);}}> <img src={slideImg3} alt='' /> </button>
+            <div className='flex flex-col justify-between items-center h-40 lg:h-96 w-1/4'>
             <p>Formação profissional!</p>
+            <button className='w-2/3 hover:w-3/4 hover:lg:transition-all h-fit flex flex-col' onClick={() => { allClosed ? setOpen3(true) : open3 ? setOpen3(false) : setTimeout(function(){setOpen3(true)}, 450); 
+            setOpen1(false);setOpen2(false);setOpen4(false);}}> <img src={slideImg3} alt='' /><p className='text-white mt-2 text-xs lg:text-base hover:bg-green-500 hover:lg:transition-colors rounded-xl pl-2 pr-2 bg-blue-800 w-fit place-self-center'>SAIBA +</p></button>
             </div>
-            <div className='flex justify-between flex-col items-center h-52 lg:h-96 w-1/4'>
-            <button className='lg:w-2/3 hover:w-3/4 hover:transition-all h-fit' onClick={() => { allClosed ? setOpen4(true) : open4 ? setOpen4(false) : setTimeout(function(){setOpen4(true)}, 450); 
-            setOpen2(false);setOpen3(false);setOpen1(false);}}><img src={slideImg4} alt='' /></button>
+            <div className='flex flex-col justify-between items-center h-40 lg:h-96 w-1/4'>
             <p>Remição da pena pelo estudo</p>
+            <button className='w-2/3 hover:w-3/4 hover:lg:transition-all h-fit flex flex-col' onClick={() => { allClosed ? setOpen4(true) : open4 ? setOpen4(false) : setTimeout(function(){setOpen4(true)}, 450); 
+            setOpen2(false);setOpen3(false);setOpen1(false);}}><img src={slideImg4} alt='' /><p className='text-white mt-2 text-xs hover:bg-green-500 hover:lg:transition-colors lg:text-base rounded-xl pl-2 pr-2 bg-blue-800 w-fit place-self-center'>SAIBA +</p></button>
             </div>
         </div>
         <Collapse isOpened={open1}>
-          <Accordion className='p-4 border-2 border-black mt-2 max-w-5xl'>
+          <Accordion className='p-4 border-4 border-gray-400 mt-2 max-w-5xl'>
           <AccordionDetails className='text-lg mt-2'>{listText1.map((item, key)=>{return <li>{item.text}</li>})}
           </AccordionDetails>
           </Accordion>
         </Collapse>
         <Collapse isOpened={open2}>
-          <Accordion className='p-4 border-2 border-black mt-2 max-w-5xl'>
+          <Accordion className='p-4 border-4 border-gray-400 mt-2 max-w-5xl'>
           <AccordionDetails className='text-lg mt-2'>{listText2.map((item, key)=>{return <li>{item.text}</li>})}
           </AccordionDetails>
           </Accordion>
         </Collapse>
         <Collapse isOpened={open3}>
-          <Accordion className='p-4 border-2 border-black mt-2 max-w-5xl'>
+          <Accordion className='p-4 border-4 border-gray-400 mt-2 max-w-5xl'>
           <AccordionDetails className='text-lg mt-2'>{listText3.map((item, key)=>{return <li>{item.text}</li>})}
           </AccordionDetails>
           </Accordion>
         </Collapse>
         <Collapse isOpened={open4}>
-          <Accordion className='p-4 border-2 border-black mt-2 max-w-5xl'>
+          <Accordion className='p-4 border-4 border-gray-400 mt-2 max-w-5xl'>
           <AccordionDetails className='text-lg mt-2'>{listText4.map((item, key)=>{return <li>{item.text}</li>})}
           </AccordionDetails>
           </Accordion>
