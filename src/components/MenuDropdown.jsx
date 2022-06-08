07a06dd9c0f5be65f9c7329dc647a85c8d5a2d08
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-06-08 15:31:55 -0300

    correction top

--- src/components/MenuDropdown.jsx
+++ src/components/MenuDropdown.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 export default function MenuDropdown({collapse, visible}) {
   return (
-    <div className={`bg-[#F6B112] top-52 left-44 text-center ${visible ? 'lg:block md:block block' : 'hidden'} md:hover:block lg:hover:block fixed ${collapse ? 'lg:top-10' : 'lg:top-40'} md:top-56 rounded-b-lg mt-1 p-4 md:left-48 lg:left-72`}>
+    <div className={`bg-[#F6B112] top-52 left-44 text-center ${visible ? 'lg:block md:block block' : 'hidden'} md:hover:block lg:hover:block fixed ${collapse ? 'lg:top-12' : 'lg:top-40'} md:top-56 rounded-b-lg mt-1 p-4 md:left-48 lg:left-72`}>
         <ul className=''>
             <Link to='passoapasso'>
               <li className='p-2 text-base font-bold h-auto transition-colors ease-in delay-70 flex justify-center items-center hover:bg-blue-800 hover:text-white rounded-xl'>Passo a Passo</li>
