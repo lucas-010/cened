9a746b861b93f9fe8420eb8e6cb2ed6785724c58
vctg1 <victormourago@gmail.com>
2022-06-10 19:27:05 -0300

    'finish-list-buttons'

--- src/components/CourseBox.jsx
+++ src/components/CourseBox.jsx
@@ -5,6 +5,7 @@ import { MdCheckBox } from 'react-icons/md'
 export default function CourseBox({id, image ,title, time, price, addCourseList, removeCourseList}) {
     const [selectBox, setSelectBox] = useState(false)
     const [visibleContent, setVisibleContent] = useState(false)
+    let [buttonId, setButtonId] = useState('')
   return (
     <div className={`flex flex-col w-11/12 mt-10 items-center transition-all bg-gray-300 rounded-xl p-4`}>
         <div className='flex lg:flex-row flex-col items-center'>
@@ -27,19 +28,30 @@ export default function CourseBox({id, image ,title, time, price, addCourseList,
         <div>
         </div>
         </div>
-        <div className={`${visibleContent ? 'flex' : 'hidden h-0'} transition-all justify-between w-full`}>
+        <div className={`${visibleContent ? 'flex flex-col' : 'hidden'} transition-all justify-between w-full`}>
             <div className='flex justify-between items-center flex-col text-white lg:flex-row w-full h-full'>
-                <div className='w-11/12 bg-[#888787] text-base font-semibold md:h-1/4 lg:w-1/5 rounded-lg p-4 text-center mt-5 lg:mt-0 lg:mr-4 cursor-pointer hover:border-black hover:bg-transparent border-2 border-transparent hover:text-black transition-colors'>
-                    <span>Objetivo Geral</span>
-                </div>
+                <button onClick={()=>{buttonId===1 ? setButtonId(''): setButtonId(1)}} className={`${buttonId ===1 ? 'bg-transparent border-gray-900 text-black' :'bg-[#888787]'} w-11/12 text-base font-semibold md:h-1/4 lg:w-1/5 rounded-lg p-4 text-center mt-5 lg:mt-0 lg:mr-4 cursor-pointer hover:border-black hover:bg-transparent border-2 border-transparent hover:text-black transition-colors`}>
+                    Objetivo Geral  
+                </button>
 
-                <div className='w-11/12 bg-[#888787] font-semibold md:h-1/4 lg:w-1/5 rounded-lg p-4 text-center mt-5 lg:mt-0 text-base lg:mr-4 cursor-pointer hover:border-black hover:bg-transparent border-2  border-transparent hover:text-black transition-colors'>
-                    <span>Objetivos de Aprendizagem</span>
-                </div>
+                <button onClick={()=>{buttonId===2 ? setButtonId(''): setButtonId(2)}} className={`${buttonId ===2 ? 'bg-transparent border-gray-900 text-black' :'bg-[#888787]'} w-11/12 text-base font-semibold md:h-1/4 lg:w-1/5 rounded-lg p-4 text-center mt-5 lg:mt-0 lg:mr-4 cursor-pointer hover:border-black hover:bg-transparent border-2 border-transparent hover:text-black transition-colors`}>
+                    Objetivos de Aprendizagem   
+                </button>
 
-                <div className='w-11/12 bg-[#888787] font-semibold md:h-1/4 lg:w-1/5 rounded-lg p-4 text-center mt-5 lg:mt-0 text-base lg:mr-4 cursor-pointer hover:border-black hover:bg-transparent border-2  border-transparent hover:text-black transition-colors'>
-                <span>Conteúdo Programático</span>
-                </div>
+                <button onClick={()=>{buttonId===3 ? setButtonId(''): setButtonId(3)}} className={`${buttonId ===3 ? 'bg-transparent border-gray-900 text-black' :'bg-[#888787]'} w-11/12 text-base font-semibold md:h-1/4 lg:w-1/5 rounded-lg p-4 text-center mt-5 lg:mt-0 lg:mr-4 cursor-pointer hover:border-black hover:bg-transparent border-2 border-transparent hover:text-black transition-colors`}>
+                    Conteúdo Programático   
+                </button>
+            </div>
+            <div className={`${buttonId ===1 ? '' : 'hidden'}`}>
+                <p className='lg:max-w-5xl'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            <div className={`${buttonId ===2 ? '' : 'hidden'}`}>
+                <p className='lg:max-w-5xl'>
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            </div>
+            <div className={`${buttonId ===3 ? '' : 'hidden'}`}>
+                <p className='lg:max-w-5xl'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
             </div>
         </div>
     </div>
