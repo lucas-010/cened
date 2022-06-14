f24a54a433726534dd1a78db78a2516477360f74
vctg1 <victormourago@gmail.com>
2022-06-14 15:44:45 -0300

    change-button-place

--- src/components/CoursesSelect.jsx
+++ src/components/CoursesSelect.jsx
@@ -5,7 +5,7 @@ export default function CoursesSelect({courses, totalPrice, setCoursesSelected})
   let map = courses.map(item =>item);
   let filter = map.splice(0,3);
   return (
-    <div className={`${!courses.length > 0 ? 'invisible' : false} bg-[#D1D5DB] lg:fixed lg:overflow-auto right-0 mt-5 lg:mt-0 p-4 rounded-xl md:w-2/3 w-11/12 lg:mr-20 lg:w-1/4`}>
+    <div className={`${!courses.length > 0 ? 'invisible' : false} bg-[#D1D5DB] lg:fixed lg:right-20 mt-5 lg:mt-0 p-4 rounded-xl md:w-2/3 w-11/12 lg:w-1/4`}>
         <h2 className='text-3xl text-center flex items-center justify-center font-bold'><BsCart4 size={40}/>Carrinho:</h2>
         <ul className='flex flex-col ml-5 list-outside list-decimal'>
             {filter.map((c, key)=>{
@@ -21,8 +21,8 @@ export default function CoursesSelect({courses, totalPrice, setCoursesSelected})
         </ul>
         <span className='text-2xl font-semibold'>Total: R${totalPrice},00</span>
         <div className='mt-5 flex flex-col lg:flex-row justify-between'>
-          <button onClick={()=> setCoursesSelected([])} className='bg-red-600 text-white mt-5 lg:mt-0 text-2xl font-semibold rounded-lg p-2'>Limpar Carrinho</button>
-          <button className='bg-yellow-400 text-2xl ml-5 font-semibold rounded-lg p-2'>Finalizar Compra</button>
+          <button onClick={()=> setCoursesSelected([])} className='bg-red-600 text-white text-2xl font-semibold rounded-lg p-2'>Limpar Carrinho</button>
+          <button className='bg-yellow-400 text-2xl lg:ml-5 mt-5 lg:mt-0 font-semibold rounded-lg p-2'>Finalizar Compra</button>
         </div>
     </div>
   )
