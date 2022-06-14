944be88a488d80201198581ecaee4fbd9c60598e
vctg1 <victormourago@gmail.com>
2022-06-14 11:01:46 -0300

    footer-get-year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='flex flex-col mt-24 bg-gray-600 text-center justify-center text-white h-40 w-screen bottom-0'>
-			<p>ESCOLA CENED © 2022. Todos os direitos reservados.</p>
+			<p>ESCOLA CENED © {(new Date()).getFullYear()}. Todos os direitos reservados.</p>
     </div>
   )
 }
