0eefeaa9a893aabc2a4c6a38e0a0919f326e48fd
vctg1 <victormourago@gmail.com>
2022-06-15 04:08:46 -0300

    finish-changes

--- src/components/CoursesSelect.jsx
+++ src/components/CoursesSelect.jsx
@@ -20,9 +20,9 @@ export default function CoursesSelect({courses, totalPrice, setCoursesSelected,s
         {courses.length > 3 ? <div className='lg:hidden flex justify-end self-end md:p-0 p-1'><BsFillArrowDownCircleFill size='2rem' onClick={()=>{document.getElementById('cart').scrollBy(0, 150);}}/></div>: ''}
         {courses.length ? <span className='text-2xl font-semibold'>Total: R${totalPrice},00</span>:''}
         <div className='mt-5 flex flex-col md:flex-row justify-around'>
-          <button onClick={()=> {setCoursesSelected([]);setOpenCart(false)}} className='bg-red-600 text-white xl:text-lg font-semibold rounded-lg p-2 md:p-1 lg:p-2'>Limpar Carrinho</button>
+          <button onClick={()=> {setCoursesSelected([]);setOpenCart(false)}} className={`${!courses.length ? 'hidden':''} bg-red-600 text-white xl:text-lg font-semibold rounded-lg p-2 md:p-1 lg:p-2`}>Limpar Carrinho</button>
           <button onClick={()=> {setOpenCart(false)}} className='lg:hidden text-white bg-green-400 md:ml-5 mt-3 md:mt-0 font-semibold rounded-lg p-2 md:p-1'>Continuar comprando</button>
-          <button className='bg-yellow-400 xl:text-lg md:ml-5 mt-3 md:mt-0 font-semibold rounded-lg p-2 md:p-1 lg:p-2'>Finalizar Compra</button>
+          <button className={`${!courses.length ? 'hidden':''} bg-yellow-400 xl:text-lg md:ml-5 mt-3 md:mt-0 font-semibold rounded-lg p-2 md:p-1 lg:p-2`}>Finalizar Compra</button>
         </div>
     </div>
   )
