4eaa895dae6a950ef57930eccd917932f90065a7
vctg1 <victormourago@gmail.com>
2022-06-21 17:50:16 -0300

    mobile-fix-studentArea

--- src/components/AllCourses.jsx
+++ src/components/AllCourses.jsx
@@ -19,9 +19,9 @@ export default function AllCourses({data}){
             <h1 className='flex p-10 justify-center items-center text-2xl font-bold'>Todos os cursos</h1>
             <div className='p-2 border-gray-400'>
             {data.map(item=>
-            <div className='flex flex-col lg:p-1 lg:border-t-2 border-gray-400'>
-                <ul className='flex lg:gap-6'><li>Curso: <b>{item.curso.codigo}</b> - {item.curso.nome}</li> <li>CargaHorária: <b>{item.curso.cargaHoraria}</b></li></ul>
-                <ul className='flex lg:gap-6'><li>Matrícula: <b>{item.numeroMatricula ? item.numeroMatricula : 'N/D'}</b></li><li>Início do Curso: <b>{item.inicioCurso ? new Date(item.inicioCurso).toLocaleDateString('pt-BR'):'Aguardando'}</b></li>
+            <div className='flex flex-col lg:p-1 border-t-2 border-gray-400'>
+                <ul className='flex flex-col lg:flex-row lg:gap-6'><li>Curso: <b>{item.curso.codigo}</b> - {item.curso.nome}</li> <li>CargaHorária: <b>{item.curso.cargaHoraria}</b></li></ul>
+                <ul className='flex flex-col lg:flex-row lg:gap-6'><li>Matrícula: <b>{item.numeroMatricula ? item.numeroMatricula : 'N/D'}</b></li><li>Início do Curso: <b>{item.inicioCurso ? new Date(item.inicioCurso).toLocaleDateString('pt-BR'):'Aguardando'}</b></li>
                 <li>Término: <b>{item.terminoCurso ? new Date(item.terminoCurso).toLocaleDateString('pt-BR') : 'Indefinido'}</b></li><li>Financeiro: <b>{item.statusFinanceiroDescricao}</b></li>
                 <li>Situação: <b>{item.statusCursoDescricao}</b></li>
                 </ul>
