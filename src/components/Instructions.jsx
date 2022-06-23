ca77c109eeadc1a07d0f1fdb2578565458f393cc
Lucas Pereira <65687729+lucas-010@users.noreply.github.com>
2022-06-23 16:09:14 -0300

    fonts workWithUs equalized

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -11,9 +11,9 @@ export default function Instructions() {
 	]
   return (
     <div className='bg-[#EBECF0] w-11/12 p-4 rounded-xl mt-0 m-auto'>
-      <span className='font-semibold text-lg'>Amigo(a) interessado(a),</span>
-			<p className='text-base mt-5'>Seja você pessoa FÍSICA ou JURÍDICA, convidamos-lhe a trabalhar conosco como parceiro no marketing e divulgação do nosso Programa de Qualificação Profissional as Pessoas Privadas de Liberdade, aplicado nos estados. Faça contato, por telefone ou via e-mail. No e-mail, escreva:</p>
-			<ol className='mt-5 text-2xl'>
+      <span className='font-semibold text-xl'>Amigo(a) interessado(a),</span>
+			<p className='text-xl mt-5'>Seja você pessoa FÍSICA ou JURÍDICA, convidamos-lhe a trabalhar conosco como parceiro no marketing e divulgação do nosso Programa de Qualificação Profissional as Pessoas Privadas de Liberdade, aplicado nos estados. Faça contato, por telefone ou via e-mail. No e-mail, escreva:</p>
+			<ol className='mt-5 text-xl'>
 				{listContent.map((text, key)=>{
 					return(
 						<li key={key} className='mt-3'>{key+1}. {text}</li>
